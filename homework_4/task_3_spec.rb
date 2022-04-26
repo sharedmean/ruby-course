5a977eec0c8d7b1d18495318e92a26b16e932041
Julia <54768451+sharedmean@users.noreply.github.com>
2022-04-26 19:18:54 +0300

    Create task_3_spec.rb

---
+++ homework_4/task_3_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'rspec'
+require './task_3'
+
+RSpec.describe 'Task_3' do
+  it "allows user to make a withdrawal and returns renewed balance" do
+    allow_any_instance_of(Kernel).to receive(:gets).and_return(50)
+
+    expect(withdraw(100)).to eq(50)
+  end
+end
