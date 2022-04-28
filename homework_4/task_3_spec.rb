77dff2fc622e9b567800018ebabe63388a67ac1e
Julia Skorobogatova <skor.julia@bk.ru>
2022-04-28 15:34:11 +0300

    updated test params, but still not working :(

--- homework_4/task_3_spec.rb
+++ homework_4/task_3_spec.rb
@@ -5,7 +5,7 @@ require './task_3'
 
 RSpec.describe 'Task_3' do
   it "allows user to make a withdrawal and returns renewed balance" do
-    allow_any_instance_of(Kernel).to receive(:gets).and_return(50)
+    allow_any_instance_of(Kernel).to receive(:gets).and_return('W', 50, 'Q')
 
     expect(withdraw(100)).to eq(50)
   end
