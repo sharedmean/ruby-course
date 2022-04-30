09e519d061cbb755327e6d09be6c13855b4867ae
Julia Skorobogatova <skor.julia@bk.ru>
2022-04-30 16:28:34 +0300

    added test for task_3

--- homework_4/task_3.rb
+++ homework_4/task_3.rb
@@ -6,6 +6,32 @@ COMMANDS = "Available commands: \n Enter 'D' to deposit money" \
 "\n Enter 'W' to withdraw money \n Enter 'B' to check your balance" \
 "\n Enter 'Q' to quit \n"
 
+def run
+  File.write(BALANCE, START_BALANCE) unless File.exist?(BALANCE)
+  balance = File.read(BALANCE).to_f
+  puts("Hello! Your current balance is #{balance} \n")
+  command = ''
+  print COMMANDS
+  loop do
+    print 'Enter your next command: '
+    command = gets.chomp
+    if %w[D d].include?(command) # %w[D d] = ['D', 'd'] - rubocop's will
+      balance = deposit(balance)
+    elsif %w[W w].include?(command)
+      balance = withdraw(balance)
+    elsif %w[B b].include?(command)
+      print "Your current balance is #{balance} \n"
+    elsif %w[Q q].include?(command)
+      print "Your current balance is #{balance}. Have a nice day! \n"
+      File.write(BALANCE, balance)
+      break
+    else
+      print 'Wrong command! '
+      print COMMANDS
+    end
+  end
+end
+
 def deposit(balance)
   print 'Please, enter your amount > 0 : '
   amount = gets.to_f
@@ -34,26 +60,4 @@ def withdraw(balance)
   balance
 end
 
-File.write(BALANCE, START_BALANCE) unless File.exist?(BALANCE)
-balance = File.read(BALANCE).to_f
-puts("Hello! Your current balance is #{balance} \n")
-command = ''
-print COMMANDS
-loop do
-  print 'Enter your next command: '
-  command = gets.chomp
-  if %w[D d].include?(command) # %w[D d] = ['D', 'd'] - rubocop's will
-    balance = deposit(balance)
-  elsif %w[W w].include?(command)
-    balance = withdraw(balance)
-  elsif %w[B b].include?(command)
-    print "Your current balance is #{balance} \n"
-  elsif %w[Q q].include?(command)
-    print "Your current balance is #{balance}. Have a nice day! \n"
-    File.write(BALANCE, balance)
-    break
-  else
-    print 'Wrong command! '
-    print COMMANDS
-  end
-end
+run if ENV != 'test'
--- homework_4/task_3_spec.rb
+++ homework_4/task_3_spec.rb
@@ -1,12 +1,23 @@
 # frozen_string_literal: true
 
+ENV = 'test'
+
 require 'rspec'
 require './task_3'
 
 RSpec.describe 'Task_3' do
-  it "allows user to make a withdrawal and returns renewed balance" do
-    allow_any_instance_of(Kernel).to receive(:gets).and_return('W', 50, 'Q')
+  it 'allows user to make a withdrawal and returns renewed balance' do
+    allow_any_instance_of(Kernel).to receive(:gets).and_return(50, 170, 'One hundred')
 
     expect(withdraw(100)).to eq(50)
+    expect(withdraw(100)).to eq(100)
+    expect(withdraw(100)).to eq(100)
+  end
+
+  it 'allows user to make a deposit and returns renewed balance' do
+    allow_any_instance_of(Kernel).to receive(:gets).and_return(100, 'One hundred')
+
+    expect(deposit(100)).to eq(200)
+    expect(deposit(100)).to eq(100)
   end
 end
